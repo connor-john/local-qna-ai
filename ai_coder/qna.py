e97501065fecd87b041a42ab11ab3dc9c1dd9ac9
Connor (Cub) <connor@cubdigital.co.nz>
2024-04-09 11:06:50 +1200

    feat: improve docstrings, imports

--- ai_coder/qna.py
+++ ai_coder/qna.py
@@ -1,7 +1,6 @@
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, OpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import TextLoader
 from dotenv import load_dotenv, find_dotenv
@@ -10,7 +9,7 @@ load_dotenv(find_dotenv())
 
 
 class AskBot:
-    """"""
+    """The QnA bot based on OpenAI and Chroma, with Langchain for tooling."""
 
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
@@ -28,10 +27,10 @@ class AskBot:
         )
 
     def split_text(self, documents: TextLoader):
-        """"""
+        """Generic splitter for seperating text into chunks for related retrieval."""
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         return text_splitter.split_documents(documents)
 
     def query(self, query: str):
-        """"""
+        """The actual QnA step following the invoke command."""
         return self.bot.invoke(query)
